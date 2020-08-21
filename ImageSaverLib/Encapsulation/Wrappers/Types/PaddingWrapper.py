db1e510f35b7b6736dbf75b987f8930397d405b4
Florian Sauer <sauer.florian@gmx.de>
2020-08-21 15:03:29 +0200

    fix missed porting from pycryptodome to cryptography

--- ImageSaverLib/Encapsulation/Wrappers/Types/PaddingWrapper.py
+++ ImageSaverLib/Encapsulation/Wrappers/Types/PaddingWrapper.py
@@ -1,43 +1,29 @@
+from cryptography.hazmat.primitives import padding
+
 from ..BaseWrapper import BaseWrapper
 from ..WrapperErrors import UnWrapError
-from Crypto.Util.Padding import pad, unpad
-import struct
 
 
 class PaddingWrapper(BaseWrapper):
-    _wrapper_type = 'pad'
+    _wrapper_type = 'pkcs7pad'
 
-    PKCS7 = 1
-    ISO7816 = 2
-    X923 = 3
-
-    PADDING_NAMES = {
-        PKCS7: 'pkcs7',
-        ISO7816: 'iso7816',
-        X923: 'x923'
-    }
-
-    _byte_struct = struct.Struct('!B')
-    _byte_struct_len = 1
-
-    _int_struct = struct.Struct('!H')
-    _int_struct_len = 2
-
-    def __init__(self, block_len=16, algo=PKCS7):
-        if not 16 <= block_len <= 65535:
-            raise ValueError("requires 0 <= block_len <= 65535")
-        self.default_block_len = block_len
-        self.default_algo = algo
-        self.default_algo_name = self.PADDING_NAMES[self.default_algo]
-        self.default_padding_bytes = self._byte_struct.pack(self.default_algo)
-        self.default_block_len_bytes = self._int_struct.pack(self.default_block_len)
+    def __init__(self, block_len=128):
+        if not block_len % 8 or not 0 < block_len < 2040:
+            raise ValueError("block size is not a multiple of 8 or is not between 0 and 2040")
+        self.block_len = block_len
+        self.padder = padding.PKCS7(128)
 
     def wrap(self, data):
-        return self.default_padding_bytes+self.default_block_len_bytes+pad(data, self.default_block_len, self.default_algo_name)
+        padder = self.padder.padder()
+        padded_data = padder.update(data)
+        padded_data += padder.finalize()
+        return padded_data
 
     def unwrap(self, data):
-        if len(data) > self._byte_struct_len+self._int_struct_len:
-            algo = self._byte_struct.unpack(data[0:self._byte_struct_len])[0]
-            block_len = self._int_struct.unpack(data[self._byte_struct_len:self._byte_struct_len+self._int_struct_len])[0]
-            return unpad(data[self._byte_struct_len+self._int_struct_len:], block_len, self.PADDING_NAMES[algo])
-        raise UnWrapError("given data is too small")
+        padder = self.padder.unpadder()
+        unpadded_data = padder.update(data)
+        try:
+            unpadded_data += padder.finalize()
+        except ValueError:
+            raise UnWrapError("given data is too small")
+        return unpadded_data
